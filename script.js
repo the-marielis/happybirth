ba6edcb7496647b1c11fafac2c0e09c57a3999ce
Marieli Teixeira <142545847+the-marielis@users.noreply.github.com>
2023-11-28 22:58:59 -0300

    alteração

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 var gift = document.getElementById("gift");
 var botao = document.getElementById("button");
 var msgInicial = document.getElementById("msgInicial");
+var next = document.getElementById("nextButton");
 
 function mostrarMensagem() {
     document.getElementById("mensagem").style.display = "block";
     gift.style.display = 'none';
     botao.style.display = 'none';
     msgInicial.style.display = 'none';
+    document.getElementById("nextButton").style.display = "block";
+}
+
+function textao() {
+    document.getElementById("textao").style.display = "block";
+    next.style.display = 'none';
+    document.getElementById("mensagem").style.display = "none";
 }
